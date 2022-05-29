81f4132bc7ef82742938d7080a9d59881add1665
Tom McLoughlin <thomas@springsy.co.uk>
2022-05-29 18:01:37 +0100

    remove whitespace

--- main.py
+++ main.py
@@ -16,48 +16,48 @@ if len(sys.argv) < 2:
 
 def main():
     certificate_dir = sys.argv[1]
-   
+
     cf = CloudFlare.CloudFlare(token=cf_api_key)
-    
+
     try:
         zones = cf.zones.get(params = {'name':zone_name,'per_page':1})
     except CloudFlare.exceptions.CloudFlareAPIError as e:
         exit('/zones.get %d %s - api call failed' % (e, e))
     except Exception as e:
         exit('/zones.get - %s - api call failed' % (e))
-        
+
     if len(zones) == 0:
         exit('No zones found')
-        
+
     zone = zones[0]
     zone_id = zone['id']
-        
+
     with open(certificate_dir + '/cert.pem', 'rb') as f:
         dane_ee = x509.load_pem_x509_certificate(f.read(), default_backend())
-    
+
     dane_ee_pubkey = dane_ee.public_key()
     dane_ee_pubkey_bytes = dane_ee_pubkey.public_bytes(Encoding.DER, PublicFormat.SubjectPublicKeyInfo)
     digest_ee = hashlib.sha256(dane_ee_pubkey_bytes).hexdigest()
-    
+
     with open(certificate_dir + '/chain.pem', 'rb') as t:
         dane_ta = x509.load_pem_x509_certificate(t.read(), default_backend())
-        
+
     dane_ta_pubkey = dane_ta.public_key()
     dane_ta_pubkey_bytes = dane_ta_pubkey.public_bytes(Encoding.DER, PublicFormat.SubjectPublicKeyInfo)
     digest_ta = hashlib.sha256(dane_ta_pubkey_bytes).hexdigest()
-    
+
     # check/create TLSA records & update accordingly if exist
     params = {
         "type": "TLSA",
     }
-    
+
     try:
         records = cf.zones.dns_records.get(zone_id, params=params)
     except CloudFlare.exceptions.CloudFlareAPIError as e:
         exit('/zones.get - %s - api call failed' % (e))
     except Exception as e:
         exit('/zones.get - %s - api call failed' % (e))
-        
+
     for i in records:
         if "2 1 1" in i['content']:
             # update DANE_TA record
@@ -77,7 +77,7 @@ def main():
                 create_record(cf, zone_name, zone_id, 3, 1, 1, digest_ee)
 
     exit(0)
-    
+
 def update_records(cf, zone_name, zone_id, usage, selector, matching_type, certificate, record_id):
     dns_record = {
         'type':'TLSA',
@@ -90,12 +90,12 @@ def update_records(cf, zone_name, zone_id, usage, selector, matching_type, certi
             'certificate':certificate,
         },
     }
-    
+
     try:
         dns_record = cf.zones.dns_records.put(zone_id, record_id, data=dns_record)
     except CloudFlare.exceptions.CloudFlareAPIError as e:
         exit('/zones.dns_records.put %s - %d %s - api call failed' % (zone_name, e, e))
-        
+
 def create_record(cf, zone_name, zone_id, usage, selector, matching_type, certificate):
     dns_record = {
         'type':'TLSA',
@@ -108,7 +108,7 @@ def create_record(cf, zone_name, zone_id, usage, selector, matching_type, certif
             'certificate':certificate,
         },
     }
-    
+
     try:
         r = cf.zones.dns_records.post(zone_id, data=dns_record)
         print("Successfully created record: " + certificate)
